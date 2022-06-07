de586210b170a0b53441c18a574f037a7ddf36e5
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-06-07 11:40:03 +0530

    def is_ugly(n):     while n%2==0:         n=n//2     if n==1:         return True     while n%3==0:         n=n//3     if n==1:         return True     while n%5==0:         n=n//5     if n==1:         return True     return False n=int(input()) if (is_ugly(n)):     print("Ugly Number") else:     print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,19 @@
+def is_ugly(n):
+    while n%2==0:
+        n=n//2
+    if n==1:
+        return True
+    while n%3==0:
+        n=n//3
+    if n==1:
+        return True
+    while n%5==0:
+        n=n//5
+    if n==1:
+        return True
+    return False
+n=int(input())
+if (is_ugly(n)):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
