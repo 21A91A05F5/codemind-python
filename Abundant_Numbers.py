c897630133bb2eee8a3df196e462cb4871f87acb
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-10-15 15:43:57 +0530

    n=int(input()) s=0 for i in range(1,n):     if n%i==0:         s+=i if s>n:     print(True) else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,12 +1,9 @@
-def factsum(n):
-    k=0
-    for i in range(1,n):
-        if n%i==0 :
-            k=k+i
-    return k
 n=int(input())
-k=factsum(n)
-if k>n :
+s=0
+for i in range(1,n):
+    if n%i==0:
+        s+=i
+if s>n:
     print(True)
 else:
     print(False)
