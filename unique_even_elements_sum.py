4eecd8b7533254bdbf142f1a53e21ddf34ec8fc0
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-10-15 12:53:17 +0530

    n=int(input()) a=list(map(int,input().split())) a=set(a) k=0 for i in a:     if i%2==0 :         k=k+i print(k)

--- unique_even_elements_sum.py
+++ unique_even_elements_sum.py
@@ -1,8 +1,8 @@
 n=int(input())
 a=list(map(int,input().split()))
-b=[]
+a=set(a)
+k=0
 for i in a:
-    if i%2==0 and a.count(i)>=1 :
-        if i not in b:
-            b.append(i)
-print(sum(b))
+    if i%2==0 :
+        k=k+i
+print(k)
