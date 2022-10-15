520ddcda5df3bb0ecaf06aec5c0bba96ce5d055a
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-10-15 15:30:11 +0530

    a,b=map(int,input().split()) i=1 if(a<b):     mini=a else:     mini=b for i in range(1,mini+1):     if(a%i==0 and b%i==0):         hcf=i print(hcf)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,10 @@
 a,b=map(int,input().split())
-if a>b:
-    a,b=b,a
-for i in range(1,b):
-    if a%i==0 and b%i==0 :
-        k=i
-print(k)
+i=1
+if(a<b):
+    mini=a
+else:
+    mini=b
+for i in range(1,mini+1):
+    if(a%i==0 and b%i==0):
+        hcf=i
+print(hcf)
