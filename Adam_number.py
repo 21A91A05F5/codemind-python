e7a390eee7bed0a2aae5c8a2f9253fed8fbf9161
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-10-15 15:29:14 +0530

    n=int(input()) s=n*n sum1=0 sum2=0 while(n>0):     w=n%10     sum1=sum1*10+w     n=n//10 s1=sum1*sum1 while(s1>0):     a=s1%10     sum2=sum2*10+a     s1=s1//10 if(s==sum2):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,17 @@
-def reverse(num):
-    r=0
-    while num:
-        d=num%10
-        num//=10
-        r=r*10+d
-    return r
 n=int(input())
-m=n
 s=n*n
-k=reverse(m)
-p=k*k
-h=reverse(p)
-if h==s :
-    print(True)
+sum1=0
+sum2=0
+while(n>0):
+    w=n%10
+    sum1=sum1*10+w
+    n=n//10
+s1=sum1*sum1
+while(s1>0):
+    a=s1%10
+    sum2=sum2*10+a
+    s1=s1//10
+if(s==sum2):
+    print("True")
 else:
-    print(False)
+    print("False")
