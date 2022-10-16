ee602ee6c20dabe01d7294acc1f0eb28decc387c
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-10-16 11:58:11 +0530

    s=input() s=s.lower() k="abcdefghijklmnopqrstuvwxyz" b=[] for i in s:     if i in k and i not in b :         b.append(i) if len(b)==26 :     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,14 +1,11 @@
 s=input()
-s=s.replace(" ","")
 s=s.lower()
-c=0
+k="abcdefghijklmnopqrstuvwxyz"
 b=[]
-a="abcdefghijklmnopqrstuvwxyz"
 for i in s:
-    if i in a and i not in b:
+    if i in k and i not in b :
         b.append(i)
-        c+=1
-if(c==26):
+if len(b)==26 :
     print(True)
 else:
     print(False)
