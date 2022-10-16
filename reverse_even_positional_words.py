579beef118a58c6bf79fa5abd0694f3e20c5f188
M Sravani <97424362+21A91A05F5@users.noreply.github.com>
2022-10-16 11:25:27 +0530

    s=input() s=s.split() for i in range(len(s)):     if i%2==0:         s[i]=s[i][::-1] print(*s)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,9 +1,6 @@
-n=input()
-n=n.split()
-c=0
-for i in n:
-    if c%2==0 :
-        print(i[::-1],end=' ')
-    else:
-        print(i,end=' ')
-    c+=1
+s=input()
+s=s.split()
+for i in range(len(s)):
+    if i%2==0:
+        s[i]=s[i][::-1]
+print(*s)
